11e6ad2e41ef9cbd5158b854ce78c33322ed5cbf
Antoine Lafouasse <antoine.lafouasse@icloud.com>
2017-03-05 19:17:02 +0000

    Bugfix: convert outgoing message field to UNIX case

--- app/models/slack/OutgoingMessage.scala
+++ app/models/slack/OutgoingMessage.scala
@@ -3,7 +3,7 @@ package models.slack
 import models.slack.ResponseType.inChannel
 
 
-case class OutgoingMessage(responseType: ResponseType, text: String)
+case class OutgoingMessage(response_type: ResponseType, text: String)
 
 object OutgoingMessage {
   def apply(message: String): OutgoingMessage = OutgoingMessage(inChannel, message)
